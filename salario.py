92ca4cdd5338bbc523acbf17b1dc60ce256fa785
pedro da silva <pedrobrgamer25@gmail.com>
2025-09-01 16:33:23 -0300

    adiciona calculo de salario

---
+++ salario.py
@@ -0,0 +1,6 @@
+valor = float(input("Insira o valor da hora trabalhada do funcionario: "))
+horas = float(input("Horas trabalhadas no mês: "))
+
+salario = ( horas * valor )
+
+print ("O salário mensal do funcionario é de: ", salario)
